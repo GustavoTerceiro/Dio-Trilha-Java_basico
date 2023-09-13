d7f84c0a1c3213973b3453e8c1f3693a847e7d0c
Gustavo Barros <gustavo.barroslima11@gmail.com>
2023-09-12 21:04:06 -0300

    Up

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -3,3 +3,13 @@ public class TiposVariaveis {
         System.out.println("Hello, World!");
     }
 }
+
+
+/* Esse é o exemplo,de como se faz a adaptacao de uma variavel. Essa é uma boa pratica porem a linguagem permite isso  */
+
+short numeroCurto = 1;
+int numeroNormal = numeroCurto;
+short numeroCurto2 = (short) numeroNormal; /*Esse é um casting */
+
+
+ 
