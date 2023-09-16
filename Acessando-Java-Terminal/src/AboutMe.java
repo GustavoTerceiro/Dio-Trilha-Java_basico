f2446f5efff4a4ffa43b4808aa2e7e7a3b25e6c7
Gustavo Barros <gustavo.barroslima11@gmail.com>
2023-09-15 22:59:50 -0300

    Scanner resolvido

--- Acessando-Java-Terminal/src/AboutMe.java
+++ Acessando-Java-Terminal/src/AboutMe.java
@@ -5,24 +5,24 @@ public class AboutMe {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Digite seu Nome");
+            String nome = scanner.next();
 
-        System.out.println("Digite seu Nome");
-        String nome = scanner.next();
+            System.out.println("Digite seu Sobrenome");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite seu Sobrenome");
-        String sobrenome = scanner.next();
+            System.out.println("Digite sua Idade");
+            int idade = scanner.nextInt();
 
-        System.out.println("Digite sua Idade");
-        int idade = scanner.nextInt();
-
-        System.out.println("Digite sua Altura");
-        double altura = scanner.nextDouble();
+            System.out.println("Digite sua Altura");
+            double altura = scanner.nextDouble();
 
 
-        System.out.println("Olá,me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + "anos");
-        System.out.println("Minha Altura é " + altura + "cm");
+            System.out.println("Olá,me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + "anos");
+            System.out.println("Minha Altura é " + altura + "cm");
+        }
 
         /*
          * Nesse modolo, eu estudei conceitos de como acessar o meu arquivo Java através
